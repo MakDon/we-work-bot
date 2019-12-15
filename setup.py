bc5f8febcfef6bba6bcaba02fb5e593b6ea020ba
makdon <makdon@makdon.me>
2019-12-16 00:07:34 +0800

    Update: add support for markdown

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='weworkbot',
-    version='0.1.0',
+    version='0.2.0',
 
     description='A framework for creating bot for wechat work',
     url='https://github.com/MakDon/we-work-bot',
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='wechat bot',
     packages=find_packages(exclude=("tests",)),
